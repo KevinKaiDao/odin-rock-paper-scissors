011a979bd59f8f820badcb56a4276e9b20c092ce
Kevin Dao <Kevinkaidao@gmail.com>
2024-09-19 12:07:19 -0400

    Added rnd computer choice to pick its hand

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+function getComputerChoice(){
+    let rnd = Math.floor(Math.random() * 3) + 1;
+    if (rnd == 1){
+        console.log("rock");
+    }else if (rnd == 2){
+        console.log("paper");
+    }else {
+        console.log("scissors");
+    }
+}
+
+getComputerChoice();
